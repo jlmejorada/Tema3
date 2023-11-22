863899dad4f5e9cf0128c5ade0070178bd9103f6
jlmejorada <joseluis.mejorada@iesnervion.es>
2023-11-22 08:58:08 +0100

    Ej2 Subida

--- src/ejerciciosfunciones/ejercicio2/Main.java
+++ src/ejerciciosfunciones/ejercicio2/Main.java
@@ -1,9 +1,62 @@
 package ejerciciosfunciones.ejercicio2;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Creamos una variable para los números introducidos
+		int num1=0, num2=0;
+		
+		//Creamos una variable para comprobar que el número introducido tiene el formato correcto
+		boolean bandera;
+		
+		//Creamos un escaner
+		Scanner sc = new Scanner(System.in);
+		
+		//Este bucle terminara cuando el formato del número sea el correcto
+		do {
+			bandera=false;
+			//Comprobamos que el tipo introducido es el correcto
+			try {
+				//Pedimos el primer número
+				System.out.println("Dame un número");
+				num1=sc.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("El formato no es el correcto");
+				sc.nextLine();
+				bandera=true;
+			}
+		} while(bandera);
+		
+		//Este bucle terminara cuando el formato del número sea el correcto
+		do {
+		bandera=false;
+			//Comprobamos que el tipo introducido es el correcto
+			try {
+				//Pedimos el segundo número
+				System.out.println("Dame otro número");
+				num2=sc.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("El formato no es el correcto");
+				sc.nextLine();
+				bandera=true;
+			}
+		} while(bandera);
+		
+		//Vemos si los dos números introducidos no son el mismo
+		if (num1!=num2) {
+			//Vemos que número es el mayor y el menor, y según pasamos en orden distintos a los parametros
+			if (num1<num2) {
+				Numeros.entre(num1, num2);
+			} else {
+				Numeros.entre(num2, num1);
+			}
+		} else System.out.println("Los números no pueden ser iguales");
+		
+		//Cerramos el escaner
+		sc.close();
 
 	}
 
---
+++ src/ejerciciosfunciones/ejercicio2/Numeros.java
@@ -0,0 +1,11 @@
+package ejerciciosfunciones.ejercicio2;
+
+public class Numeros {
+	public static void entre (int num1, int num2) {
+		//Creamos un bucle que recorra números hasta el número mayor
+		for (int i = (num1+1) ; i<num2 ; i++) {
+			//Imprimira por pantalla el número en el que se encuentra
+			System.out.println(i);
+		}
+	}
+}
