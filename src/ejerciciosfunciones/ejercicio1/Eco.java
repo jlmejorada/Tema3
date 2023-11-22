3f022a024fb789d74706db08a9e5b276e344975e
jlmejorada <joseluis.mejorada@iesnervion.es>
2023-11-22 08:30:02 +0100

    EJ 1 UPD

--- src/ejerciciosfunciones/ejercicio1/Eco.java
+++ src/ejerciciosfunciones/ejercicio1/Eco.java
@@ -3,6 +3,7 @@ package ejerciciosfunciones.ejercicio1;
 public class Eco {
 	
 		public static void eco(int num){
+			//Creamos un bucle que lance el eco hasta que se acabe este
 			for (int i=1; i <=num; i++) {
 				System.out.println("Eco...");
 			}
--- src/ejerciciosfunciones/ejercicio1/Main.java
+++ src/ejerciciosfunciones/ejercicio1/Main.java
@@ -1,5 +1,6 @@
 package ejerciciosfunciones.ejercicio1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -11,9 +12,19 @@ public class Main {
 		//Creamos un escaner
 		Scanner sc=new Scanner(System.in);
 		
-		//Pedimos el númeroo  de veces que queremos que repita el eco y lo recogemos
-		System.out.println("Dime cuantas veces se repetira el eco");
-		num=sc.nextInt();
+		//Hacemos un bucle que pida números hasta que se le introduzca uno mayor que 0
+		do {
+			//Comprobamos que se hayan introducido bien los caracteres
+			try {
+				//Pedimos el números  de veces que queremos que repita el eco y lo recogemos
+				System.out.println("Dime cuantas veces se repetira el eco");
+				num=sc.nextInt();
+			}catch(InputMismatchException e) {
+				//Pasamos un mensaje si vemos que no es correcto
+				System.out.println("El tipo de caracter introducido no es correcto");
+				sc.nextLine();
+			}
+		} while (num<=0);
 		
 		//Llamamos a la clase con el parametro num
 		Eco.eco(num);
