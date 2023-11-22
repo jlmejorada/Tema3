21dfce1e7133a9f48cf63cac6b99e0319d8ef456
jlmejorada <joseluis.mejorada@iesnervion.es>
2023-11-22 10:06:33 +0100

    Ej4 Subida

---
+++ src/ejerciciosfunciones/ejercicio4/Main.java
@@ -0,0 +1,46 @@
+package ejerciciosfunciones.ejercicio4;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Creamos una variable que recoja un caracter
+		char letra=' ';
+		
+		//Creamos una variable bool que sirva de bandera, para recoger el dato y otra para ver si es primo
+		boolean bandera, vocal;
+		
+		//Creamos el escaner
+		Scanner sc = new Scanner(System.in);
+		
+		// Este bucle terminara cuando el formato del número sea el correcto
+		do {
+			bandera = false;
+			// Comprobamos que el tipo introducido es el correcto
+			try {
+				// Pedimos el primer número
+				System.out.println("Dame una letra");
+				letra = sc.next().charAt(0);
+			} catch (InputMismatchException e) {
+				System.out.println("El formato no es el correcto");
+				sc.nextLine();
+				bandera = true;
+			}
+		} while (bandera);
+		
+		//Llamamos a la funcion y recogemos la salida
+		vocal = Palabra.vocal(letra);
+		
+		//Hacemos un if que nos diga si es verdadero o falso y en consecuencia, mostraremos un mensaje
+		if (vocal) {
+			System.out.println("Es una vocal");
+		} else System.out.println("No es una vocal");
+		
+		//Cerramos el escaner
+		sc.close();
+
+	}
+
+}
---
+++ src/ejerciciosfunciones/ejercicio4/Palabra.java
@@ -0,0 +1,16 @@
+package ejerciciosfunciones.ejercicio4;
+
+public class Palabra {
+
+	public static boolean vocal (char letra) {
+		boolean vocal=false;
+		
+		//Hacemos un switch que en el caso de que sea vocal, mande true
+		vocal= switch(Character.toLowerCase(letra)) {
+		case 'a','e', 'i','o', 'u'  -> true;
+		default-> false;
+		};
+		
+		return vocal;
+	}
+}
