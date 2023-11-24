a23858d6da0615fe24be82b9a3376cd89696a3ed
jlmejorada <joseluis.mejorada@iesnervion.es>
2023-11-24 12:58:49 +0100

    EJ5 UPD

--- src/ejerciciosfunciones/ejercicio5/Main.java
+++ src/ejerciciosfunciones/ejercicio5/Main.java
@@ -1,12 +1,13 @@
 package ejerciciosfunciones.ejercicio5;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// Creamos una variable para recoger un número entero
-		int num;
+		int num=0;
 		
 		//Creamos una booleana para controlar el bucle de pedida de datos
 		boolean bandera;
@@ -16,17 +17,24 @@ public class Main {
 		
 		//Creamos un  bucle para que pida el número hasta que se introduzca un caracter correcto
 		do {
+			bandera=false;
 			//Controlamos que la variable sea del tipo correcto
 			try {
-				
-			} catch () {
-				
+				//Pedimos un número y lo registramos
+				System.out.println("Dame un número y te imprimo la tabla de multiplicar");
+				num=sc.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("El tipo de dato introducido no es correcto");
+				sc.nextLine();
+				bandera=true;
 			}
-		} while (true);
+		} while (bandera);
+		
+		//Pasamos el número como parametro
+		Numero.mul(num);
 		
 		//Cerramos el escaner
 		sc.close();
-
 	}
 
 }
--- src/ejerciciosfunciones/ejercicio5/Numero.java
+++ src/ejerciciosfunciones/ejercicio5/Numero.java
@@ -2,6 +2,11 @@ package ejerciciosfunciones.ejercicio5;
 
 public class Numero {
 	public static void mul (int num) {
+		int mul=0;
+		for (int i=0 ; i<=10 ; i++) {
+			mul = num * i;
+			System.out.println(num + " * " + i + " = " + mul );
+		}
 		
 	}
 }
