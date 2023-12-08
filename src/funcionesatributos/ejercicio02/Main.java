9a92b470e91af947b8ebf1edb512d0e37846249f
mamej <mamej@MejoradaMarin>
2023-12-08 13:35:12 +0100

    EJ2 UPD

--- src/funcionesatributos/ejercicio02/Main.java
+++ src/funcionesatributos/ejercicio02/Main.java
@@ -1,22 +1,28 @@
 package funcionesatributos.ejercicio02;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
+	// Creamos el escaner
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		// Creamos una variable para recoger la opción elegida, otra para la altura,
+		// para el ancho y otra para el resultado
 		int opc;
 		double alt, anch, res = 0;
 		Rectangulo rc;
-
+		// LLamamos a la funcion que pide el ancho y lo recogemos
 		anch = pideAncho();
+		// LLamamos a la funcion que pide la áltura y lo recogemos
 		alt = pideAlto();
-
+		// Llamamos a la función que muestra el menú y nos devuelve la opción elegida
 		opc = menu();
-
 		rc = new Rectangulo(anch, alt);
+		// Ahora gracias a un switch llamaremos a la función correspondiente a la opc
+		// elegida o saldremos del programa
 		switch (opc) {
 		case 1 -> {
 			res = rc.perimetro();
@@ -28,32 +34,52 @@ public class Main {
 			System.out.println("Saliendo...");
 		}
 		}
-		
+		// mostraremos por pantalla el resultado
 		System.out.println("El resultado es " + res);
-
+		// Cerramos el escaner
 		sc.close();
 	}
 
+	// Creamos una función que mostrara un menú por pantalla y devuelve la opción
+	// elegida por el usuario
 	public static int menu() {
-		int num;
-		System.out.println("***************");
-		System.out.println("1. Perímetro");
-		System.out.println("2. Área");
-		System.out.println("0. Salir");
-		System.out.println("***************");
-		System.out.println("Elige una opción");
-		num = sc.nextInt();
-		return num;
+		// Creamos una variable para recoger el número de la opción
+		int opc = -1;
+		// Comprobamos que la opción este entre 0 y 3
+		do {
+			// Mostramos el Menú por pantalla
+			System.out.println("************");
+			System.out.println("****Menú****");
+			System.out.println("************");
+			System.out.println("1. Perimetro");
+			System.out.println("2. Área");
+			System.out.println("0. Salir");
+			System.out.println("************");
+			// Comprobamos que el tipo introducido es el correcto
+			try {
+				// Pedimos la opción
+				System.out.println("Elige una opción entre las disponibles");
+				opc = sc.nextInt();
+				// Si nos sale error
+			} catch (InputMismatchException e) {
+				// Mandamos un mensaje diciendo que el tipo es incorrecto
+				System.out.println("El formato no es el correcto");
+				sc.nextLine();
+			}
+		} while (opc < 0 || opc > 2);
+		return opc;
 	}
 
+	// Creamos una función que pedira la anchura y la devolvera
 	public static double pideAncho() {
 		System.out.println("Dame la anchura de la figura");
-		return sc.nextInt();
+		return sc.nextDouble();
 	}
 
+	// Creamos una función que pedira la altura y la devolvera
 	public static double pideAlto() {
 		System.out.println("Dame la altura de la figura");
-		return sc.nextInt();
+		return sc.nextDouble();
 	}
 
 }
--- src/funcionesatributos/ejercicio02/Rectangulo.java
+++ src/funcionesatributos/ejercicio02/Rectangulo.java
@@ -1,17 +1,21 @@
 package funcionesatributos.ejercicio02;
 
 public class Rectangulo {
+	// Creamos los atributos
 	double ancho, alto;
 
+	// Generamos un constructor con parametros
 	public Rectangulo(double ancho, double alto) {
 		this.ancho = ancho;
 		this.alto = alto;
 	}
 	
+	// Calculamos el perimetro y lo devolvemos
 	double perimetro() {
 		return ancho*2 + alto*2;
 	}
 
+	// Calculamos el área y lo devolvemos
 	double area() {
 		return ancho*alto;
 	}
